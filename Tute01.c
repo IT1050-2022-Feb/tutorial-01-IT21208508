27f1ebecd0dc0a584d489ff91be28e1f4da809a5
IT21208508_Darshi <99190916+IT21208508@users.noreply.github.com>
2022-02-08 05:39:03 +0000

    ex1 labsheet completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,13 @@
 #include <stdio.h>
 
 int main() {
+  int mark1,mark2;
+  float avg;
+  printf("Enter subject 1 marks:");
+  scanf("%d",&mark1);
+  printf("Enter subject 2 marks:");
+  scanf("%d",&mark2);
+  
   
   return 0;
 }
